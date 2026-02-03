7a1151fe4b65ec3adc0e2d7d42a6e1766a42dafb
jswessler <minerslikejs@gmail.com>
2026-02-02 19:16:27 -0500

    Gold 3
    bug fixes
    scaling plugin
    window.png changes

---
+++ js/plugins/JSW_test.js
@@ -0,0 +1,11 @@
+/*:
+
+@target Mv
+
+@plugindesc Fix those stupid options that make the game not look nice on itch.io.
+
+@author MechPen */ (() => {
+
+Graphics._defaultStretchMode = function() { return false; };
+
+})();
